4d15306e89ef8f27e18a7d685c13d450fb745e9c
JTnum4 <120874510+JTnum4@users.noreply.github.com>
2026-01-22 17:26:46 -0500

    Updated code with corrections.
    Updated code with corrections.

--- src/steam.py
+++ src/steam.py
@@ -9,23 +9,23 @@ def main():
     usersteamnumber = "ABC123"
     apikey = "ABC123"
 
-    steamgameurl = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apikey}&steamid={usersteamnumber}&format=json&include_appinfo=true"
-    response = httpx.get(steamgameurl)
+    steam_user_url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apikey}&steamid={usersteamnumber}&format=json&include_appinfo=true"
+    response = httpx.get(steam_user_url)
     user_game_data = response.json()
     #print(user_game_data["response"]["games"])
     #return(user_game_data)
     for game in user_game_data["response"]["games"]:
-        steamgamenumber = game["appid"]
+        steam_game_number = game["appid"]
         name = game["name"]
-        print(game["name"], "-", game["appid"], "-", round((game["playtime_forever"])/60, 1), "hrs",)
-        steamachieveurl = f"https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={steamgamenumber}&key={apikey}&steamid={usersteamnumber}"
-        achieve_response = httpx.get(steamachieveurl)
-        achieve_user_data = achieve_response.json()
-        playerstats = achieve_user_data.get("playerstats")
+        print(name, "-", steam_game_number, "-", round((game["playtime_forever"])/60, 1), "hrs",)
+        steam_achieve_url = f"https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={steam_game_number}&key={apikey}&steamid={usersteamnumber}"
+        achievement_response = httpx.get(steam_achieve_url)
+        achievement_user_data = achievement_response.json()
+        playerstats = achievement_user_data.get("playerstats")
         if "achievements" not in playerstats:
-            print("N/A")
+            #print("N/A")
             continue
-        print(achieve_user_data["playerstats"]["achievements"])
+        print(achievement_user_data["playerstats"]["achievements"])
         
         
     return user_game_data
